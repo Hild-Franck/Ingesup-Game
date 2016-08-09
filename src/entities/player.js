2115ba2ec3ef979becb82520102df3c9c188e338
Hild-Franck <franck.hild@ynov.com>
2016-08-09 14:13:35 +0200

    Made the character turn on 8 direction

--- src/entities/player.js
+++ src/entities/player.js
@@ -1,12 +1,17 @@
+const toDirection = require('../utils/toDirection')
 const initPlayer = sprite => {
+	sprite.anchor.setTo(0.5, 0.5)
 	const player = {
 		x: 0,
 		y: 0,
 		speed: 4,
+		dir: 90,
 		sprite,
 		move: ({ x, y }) => {
 			player.x += x
 			player.y += y
+			player.dir = toDirection(player.dir, x, y)
+			player.sprite.angle = player.dir
 			player.sprite.x = player.x
 			player.sprite.y = player.y
 		}
